b500a49ae6ad713f5b9c5cef50e24b76717562e5
Franky Erra <frankyerra43@gmail.com>
2022-08-11 20:04:06 -0700

    modified handlebars

--- public/js/updateRecipe.js
+++ public/js/updateRecipe.js
@@ -2,27 +2,26 @@ const recipeHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('#recipe-title').value.trim();
-  const ingredients = document.querySelector('#recipe-ingredients').value.trim();
-  const instructions = document.querySelector('#recipe-instructions').value.trim();
+  const content = document.querySelector('#recipe-ingredients').value.trim();
+  // const instructions = document.querySelector('#recipe-instructions').value.trim();
   const recipeId = document.querySelector('#submit').getAttribute('data-id');
 
-  if (recipeId && title && ingredients && instructions) {
-    const response = await fetch(`/api/recipes/${recipeId}`, {
+  if (recipeId && title && content) {
+    const response = await fetch(`/api/blogs/${recipeId}`, {
       method: 'PUT',
       body: JSON.stringify({ 
-        name: title, 
-        ingredients, 
-        instructions }),
+        title, 
+        content }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      alert('Your recipe has been updated.')
+      alert('Your post has been updated.')
       document.location.replace('/profile');
     } else {
-      alert('Failed to update recipe');
+      alert('Failed to update post');
     }
   }
 };

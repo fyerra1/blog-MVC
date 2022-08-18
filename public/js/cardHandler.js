7a7b4cc4c3e80de96f93844c76f861de99ce5802
Franky Erra <frankyerra43@gmail.com>
2022-08-17 23:20:51 -0700

    fixed frontend profile functions

--- public/js/cardHandler.js
+++ public/js/cardHandler.js
@@ -2,12 +2,7 @@ const cardHandler = async (event) => {
     event.preventDefault();
 
     const id = event.target.getAttribute('data-id');
-    // const recipeEditId = event.target.getAttribute('data-edit-id');
     const cardRecipeId = event.target.getAttribute('data-card-id');
-
-    // if(cardRecipeId){
-    //     document.location.replace(`/update-recipes/${cardRecipeId}`);
-    // } else console.log(cardRecipeId);
 
     if (id) {
         console.log(id)
@@ -20,9 +15,6 @@ const cardHandler = async (event) => {
         } else {
             alert('Failed to delete project');
         }
-    // } else if (recipeEditId) {
-    //     console.log(recipeEditId);
-    //     document.location.replace(`/view-recipe/${recipeEditId}`);
     } else if (cardRecipeId){
         console.log(cardRecipeId);
         document.location.replace(`/update-post/${cardRecipeId}`);
@@ -33,9 +25,3 @@ const cardHandler = async (event) => {
 document
   .querySelector('.project-list')
   .addEventListener('click', cardHandler);
-
-     // if(id) {
-    //     document.location.replace(`/update-recipes/${id}`);
-    // } else {
-    //     console.log(id);
-    // }

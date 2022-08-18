454fab56bf078da87d55366e7cced88b7c0c2cdf
Franky Erra <frankyerra43@gmail.com>
2022-08-17 20:51:57 -0700

    original version

--- public/js/cardHandler.js
+++ public/js/cardHandler.js
@@ -1,34 +1,34 @@
 const cardHandler = async (event) => {
     event.preventDefault();
 
-    const id = event.target.getAttribute('data-edit-id');
-    // const recipeEditId = event.target.getAttribute('data-edit-id');
-    // const cardRecipeId = event.target.getAttribute('data-card-id');
+    const id = event.target.getAttribute('data-id');
+    const recipeEditId = event.target.getAttribute('data-edit-id');
+    const cardRecipeId = event.target.getAttribute('data-card-id');
 
-    if(id) {
-        document.location.replace(`/update-recipes/${id}`);
-    } else {
-        console.log(id);
-    }
-
-    // if (id) {
-
-    //     const response = await fetch(`api/blogs/${id}`, {
-    //         method: 'DELETE',
-    //     });
-    
-    //     if (response.ok) {
-    //         document.location.replace('/profile');
-    //     } else {
-    //         alert('Failed to delete project');
-    //     }
-    // } else if (recipeEditId) {
-
-    //     document.location.replace(`/update-recipes/${recipeEditId}`);
+    // if(id) {
+    //     document.location.replace(`/update-recipes/${id}`);
     // } else {
-
-    //     document.location.replace(`/update-recipe/${cardRecipeId}`);
+    //     console.log(id);
     // }
+
+    if (id) {
+        console.log(id)
+        const response = await fetch(`api/blogs/${id}`, {
+            method: 'DELETE',
+        });
+    
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete project');
+        }
+    } else if (recipeEditId) {
+
+        document.location.replace(`/update-recipes/${recipeEditId}`);
+    } else {
+
+        document.location.replace(`/view-recipe/${cardRecipeId}`);
+    }
 
 };
 
